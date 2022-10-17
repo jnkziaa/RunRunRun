46c88ad5a6387af8b3611a92f20abc624300ff4e
John Albesa <107880782+jnkziaa@users.noreply.github.com>
2022-10-16 23:27:05 -0700

    added data fields
    Took 32 minutes

--- src/main/java/com/example/runrunrun/HelloApplication.java
+++ src/main/java/com/example/runrunrun/HelloApplication.java
@@ -1,20 +1,29 @@
 package com.example.runrunrun;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Runners.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+        stage.setTitle("Kings of Runners Tracker v0.1");
         stage.setScene(scene);
         stage.show();
+
+
     }
 
     public static void main(String[] args) {
